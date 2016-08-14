2a1795949458a75d35ad03a53e19b0fe6f169b64
piotrgrudzien <piotr.szymon.grudzien@gmail.com>
2016-08-15 00:20:08 +0100

    Small fixes...

--- php/save.php
+++ php/save.php
@@ -1,8 +1,3 @@
 <?php
-header('Content-type: text/plain');
-header('Access-Control-Allow-Origin: https://piotrgrudzien.github.io');
-$myfile = fopen("output.txt", "w");
-fwrite($myfile, "Wrote something");
-fclose($myfile);
-echo $_GET["time"];
+echo $_POST["time"];
 ?>
--- php/saveUTF.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-type: text/plain');
-header('Access-Control-Allow-Origin: https://piotrgrudzien.github.io');
-$myfile = fopen("output.txt", "w");
-fwrite($myfile, "Wrote something");
-fclose($myfile);
-echo $_GET["time"];
-?>
