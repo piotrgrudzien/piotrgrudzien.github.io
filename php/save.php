843c3b1719911716fb4ade91334831b9e1516f79
piotrgrudzien <piotr.szymon.grudzien@gmail.com>
2016-08-14 23:42:17 +0100

    Small fixes...

--- php/save.php
+++ php/save.php
@@ -1,5 +1,5 @@
 header('content-type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://piotrgrudzien.github.io');
 header('Access-Control-Allow-Methods: GET, POST');
 header("Access-Control-Allow-Headers: X-Requested-With");
 echo $_POST["time"];
