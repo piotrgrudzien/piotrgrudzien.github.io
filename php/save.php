654697c38c3066069bb978d2ba96828023f103b0
piotrgrudzien <piotr.szymon.grudzien@gmail.com>
2016-08-14 23:52:38 +0100

    Small fixes...

--- php/save.php
+++ php/save.php
@@ -1,5 +1,8 @@
 <?php
 header('Content-type: text/plain');
 header('Access-Control-Allow-Origin: https://piotrgrudzien.github.io');
+$myfile = fopen("newfile.txt", "w");
+fwrite($myfile, "Wrote something");
+fclose($myfile);
 echo $_GET["time"];
 ?>
